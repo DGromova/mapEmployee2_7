80892040b21ec007382c09fab793004c8b3b88ba
Daria Gromova <igromova81@gmail.com>
2023-04-03 22:36:54 +0300

    Initial commit

---
+++ src/main/java/com/example/mapemployee2_7/MapEmployee27Application.java
@@ -0,0 +1,13 @@
+package com.example.mapemployee2_7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapEmployee27Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapEmployee27Application.class, args);
+	}
+
+}

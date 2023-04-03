9fd2b007d1a178261e3a06461c805668e3f56d32
Daria Gromova <igromova81@gmail.com>
2023-04-03 22:38:18 +0300

    Start

--- src/main/java/com/example/mapemployee2_7/MapEmployee27Application.java
+++ src/main/java/com/example/mapemployee2_7/MapEmployee27Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MapEmployee27Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MapEmployee27Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MapEmployee27Application.class, args);
+    }
 
 }
